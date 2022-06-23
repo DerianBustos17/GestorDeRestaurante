eb57d95f6bba9a219b4ea3eac7ad385ddd21db99
Elieth Barquero <elieth.barquero@ucr.ac.cr>
2022-06-23 16:09:18 -0600

    Clase estado y clase mesas del catalogo de mesas

---
+++ GestorDeRestaurante.Model/Estado.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestorDeRestaurante.Model
+{
+    public enum Estado
+    {
+        Disponible = 1,
+        Nodisponible = 2
+    }
+}
